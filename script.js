2684a556114ca6014b836e044cc251ff2020ee13
Raul da Silva Bonelli <raulbonellimult579@gmail.com>
2026-05-09 12:09:26 -0300

    Agora ele usa uma lógica de classe, então podemos usa la para qualquer coisa

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-let img = ["../IMG/cacoma.jfif","../IMG/Villager.jfif"]
-let index = 0;
+let bannerImg = ["./IMG/cacoma.jfif","./IMG/Villager.jfif","./IMG/noBitches.jpg"]
+let comment1IMG = ["./IMG/noBitches.jpg","./IMG/cacoma.jfif","./IMG/Villager.jfif"]
+let banner = document.getElementById("carousel")
+let comment1 = document.getElementById("comment1")
+let comment2 = document.getElementById("comment2")
 
-function Switch(){
-if (index < 1){
-    index = 1
-}else{
-    index = 0
+class switchImages {
+    constructor(imgs,id) {
+        this.images = imgs;
+        this.imgsLength = this.images.length;
+        this.id = id;
+        this.index = 0;
+    }
+    switchImages(){
+        if (this.index < this.images.length -1){
+            this.index++
+        }else{
+            this.index = 0
+        }
+        this.id.src = this.images[this.index]
+        
+    }
 }
-document.getElementById("carousel").src = img[index]
 
-}
-
+let bannerAnimation = new switchImages(bannerImg,banner);
+let commentsAnimation = new switchImages(comment1IMG,comment1)
+let commentAnimation= new switchImages(bannerImg,comment2)
 
 setInterval(() => {
-    Switch()
-}, 5000);
+    bannerAnimation.switchImages();
+    commentsAnimation.switchImages();
+    commentAnimation.switchImages();
+}, 1000);
